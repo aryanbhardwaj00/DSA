edd55f290697f4880c9600c905d3ba53939cbeec
Aryan <bhardwajaryan1000@gmail.com>
2024-07-07 10:02:25 +0530

    Solved new Array Problems

--- Arrays.go
+++ Arrays.go
@@ -1,6 +1,5 @@
 package main
 
-
 // Buy a stock on ith day and sell on other day
 // Return the max profit
 // Return 0 if no profit
@@ -42,4 +41,41 @@ func maxSubArray(nums []int) int {
 		maxSoFar = max(maxSoFar, maxEndHere)
 	}
 	return maxSoFar
+}
+
+// Reverse an Array using recursion
+func revArrays(arr []int, leftIdx int, rightIdx int) {
+	if leftIdx >= rightIdx {
+		return
+	}
+	arr[rightIdx], arr[leftIdx] = arr[leftIdx], arr[rightIdx]
+	revArrays(arr, leftIdx+1, rightIdx-1)
+}
+
+// Largest element in an Array
+func largestElement(arr []int) int {
+	greatest := 0
+	for i := 0; i < len(arr); i++ {
+		if arr[i] >= greatest {
+			greatest = arr[i]
+		}
+	}
+	return greatest
+}
+
+// Second Largest Element in an Array
+func secLargest(arr []int) int {
+	largest := largestElement(arr)
+	for i := 0; i < len(arr); i++ {
+		if arr[i] >= largest {
+			arr[len(arr)-1], arr[i] = arr[i], arr[len(arr)-1]
+		}
+	}
+	secondLargest := 0
+	for i := 0; i < len(arr)-1; i++ {
+		if arr[i] >= secondLargest {
+			secondLargest = arr[i]
+		}
+	}
+	return secondLargest
 }
--- Patterns_test.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "testing"
-
-func TestSquare(t *testing.T) {
-	square()
-}
